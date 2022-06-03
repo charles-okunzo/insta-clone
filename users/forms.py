3e6ee363a1762d89ca448fc0b1f27a6e0572ea1d
Guyo <you@example.com>
2022-06-04 00:24:47 +0300

    feature -- add user models and templates

---
+++ users/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.CharField()
+    full_name = forms.CharField(max_length=50)
+
+
+    class Meta:
+        model = User
+        fields = ['email', 'full_name', 'username', 'password']
---
+++ users/views.py
@@ -0,0 +1,17 @@
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from .forms import UserRegisterForm
+
+# Create your views here.
+
+
+def register(request):
+    form = UserRegisterForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'An account for {username} has  been created successfully. You can now login')
+            return redirect('home')
+        return render(request, 'users/register.html', {'form':form})
