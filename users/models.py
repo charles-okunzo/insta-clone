b22b33bc66cb7547618ffaf5134abea694ab4d63
Guyo <you@example.com>
2022-06-04 13:23:29 +0300

    feature --modify profile page

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_pic = models.ImageField(upload_to='profile_pics/', default= 'default.jgp')
+    profile_pic = models.ImageField(upload_to='profile_pics/', default= 'default.png')
     bio = models.TextField(blank=True)
 
 
